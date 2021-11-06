84a6909c4e553f132fc2397b2c19fa40a7333fd7
Angelo Rafael <angelorpt@gmail.com>
2021-11-06 18:25:10 -0400

    add | Component Template HomeTemplate

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="component">
-    <Container>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iusto
-        totam iste aperiam a distinctio exercitationem amet soluta sint ipsam!
-        Fugiat magni suscipit ducimus cupiditate numquam iste hic, quae
-        explicabo?
-      </p>
-    </Container>
+    <HomeTemplate />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { Container } from '@/components/bosons'
+import { HomeTemplate } from '@/components/templates'
 
 export default defineComponent({
   components: {
-    Container
+    HomeTemplate
   }
 })
 </script>
