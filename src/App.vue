22c01e3805fcecc8867349fb79a701ae3d68a3f5
Angelo Rafael <angelorpt@gmail.com>
2021-11-06 16:55:18 -0400

    add | Component Atomo SocialLink

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="component">
-    <SearchForm />
+    <SocialLink name="github" to="https://github.com" icon="github.svg" />
+    <SocialLink name="twitter" to="https://twitter.com" icon="twitter.svg" />
+    <SocialLink name="facebook" to="https://facebook.com" icon="facebook.svg" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { SearchForm } from '@/components/molecules'
+import { SocialLink } from '@/components/atoms'
 
 export default defineComponent({
   components: {
-    SearchForm
+    SocialLink
   }
 })
 </script>
