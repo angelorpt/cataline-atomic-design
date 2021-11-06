77701d83775a6ce880fc16d312feeb06f5881c8c
Angelo Rafael <angelorpt@gmail.com>
2021-11-06 16:09:03 -0400

    add | Component Logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Cataline</h1>
+  <div class="component">
+    <Logo />
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue'
+import { Logo } from '@/components/atoms'
 
-export default defineComponent({})
+export default defineComponent({
+  components: {
+    Logo
+  }
+})
 </script>
+
+<style lang="scss" scoped>
+.component {
+  width: 300px;
+}
+</style>
